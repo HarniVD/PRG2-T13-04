006326856b82d5e548976c34c966c8242921d06c
Vijayan Devi Harni <s10268043@connect.np.edu.sg>
2025-01-16 00:01:20 +0800

    Finished all the Flight classes and the Airlines Class

---
+++ PRG2-T13-04/Airline.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_04
+{
+    internal class Airline
+    {
+        private string name;
+        private string code;
+        private Dictionary<string,Flight> flights;
+
+        public string Name
+        { get { return name; } set { name = value; } }
+
+        public string Code
+            { get { return code; } set { code = value; } }
+
+        public Dictionary<string,Flight> Flights
+            { get { return flights; } set { flights = value; } }
+
+        public bool AddFlight(Flight f)
+        { 
+            foreach(KeyValuePair<string,Flight> kvp in flights)
+            { if(kvp.Key!=f.FlightNumber)
+                {
+                    Flights.Add(f.FlightNumber, f);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public bool RemoveFlight(Flight f)
+        {
+            foreach (KeyValuePair<string, Flight> kvp in flights)
+            {
+                if (kvp.Key == f.FlightNumber)
+                {
+                    Flights.Remove(f.FlightNumber);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public double CalculateFee()
+        {
+            double fee = 0;
+            foreach (KeyValuePair<string, Flight> kvp in flights)
+            {
+                fee += kvp.Value.CalculateFee();
+
+            }
+            return fee;
+        }
+
+        public override string ToString()
+        {
+            string output ="Name of AirLines: "+Name+"\nAirlines Code: "+Code+"\nFlights under Airline: ";
+            foreach (KeyValuePair <string,Flight> kvp in flights)
+            { output += "\n" + kvp.Value.FlightNumber; }
+            return output;
+        }
+
+
+    }
+}
---
+++ PRG2-T13-04/LWTTFlight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_T13_04
+{
+    internal class LWTTFlight : Flight
+    {
+        private double requestFee;
+
+        public double RequestFee
+        { get { return requestFee; } set { requestFee = value; } }
+
+        public override double CalculateFee()
+        {
+            double fee = 300;
+            if (Origin == "Singapore")
+            { fee += 500 + RequestFee; }
+            else if (Destination == "Singapore")
+            { fee += 800 + RequestFee; }
+            return fee ;
+        }
+        public override string ToString()
+        { return base.ToString() + "\nFees for the FLight: " + CalculateFee(); }
+
+        public LWTTFlight() : base()
+        { }
+
+        public LWTTFlight(string n, string o, string d, DateTime e, string s) : base(n, o, d, e, s)
+        { RequestFee = 500; }
+    } 
+        
+    }
+
